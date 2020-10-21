96de78fc4c14a44516ed7acf515d40001162e8b2
aeschyllus <markanthony.lapada@tup.edu.ph>
2020-10-21 20:08:37 +0800

    Added fade-out effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,24 @@
 const intro = document.querySelector('.intro');
-const translate = document.querySelectorAll('.translate');
+const translate = intro.querySelectorAll('.translate');
+
 const section = document.querySelector('section');
 const content = document.querySelector('.content');
 const img = document.querySelector('.img-container');
-const opacity = document.querySelectorAll('.opacity');
+const opacity = section.querySelectorAll('.opacity');
 const border = document.querySelector('.border');
+
+const outro = document.querySelector('.outro');
+const inspired = outro.querySelector('h2');
+const outroOpacity = outro.querySelector('.opacity');
 
 let introHeight = intro.offsetHeight;
 let sectionHeight = section.offsetHeight;
+let outroHeight = outro.offsetHeight;
 
 window.addEventListener('scroll', () => {
     let scroll = window.pageYOffset;
     let sectionY = section.getBoundingClientRect();
+    let outroY = outro.getBoundingClientRect();
 
     intro.style.opacity = - scroll / (introHeight / 2) + 1;
 
@@ -20,12 +27,30 @@ window.addEventListener('scroll', () => {
         element.style.transform = `translateX(${scroll * rate}px)`;
     });
 
-    opacity.forEach(element => {
-        element.style.opacity = scroll / (sectionY.top + sectionHeight);
-    });
+    // Fade-in and stop parallax for about me section
+    if (sectionY.top > 0) {
+        opacity.forEach(element => {
+            element.style.opacity = scroll / (sectionY.top + sectionHeight);
+        });
 
-    content.style.transform = `translateY(${scroll / (sectionHeight + sectionY.top) * 50 - 50}px)`;
-    img.style.transform = `translateY(-${scroll / (sectionHeight + sectionY.top) * 50 - 50}px)`;
+        content.style.transform = `translateY(${scroll / (sectionHeight + sectionY.top) * 50 - 50}px)`;
+        img.style.transform = `translateY(-${scroll / (sectionHeight + sectionY.top) * 50 - 50}px)`;
 
-    border.style.width = `${scroll / (sectionY.top + sectionHeight) * 5}em`;
+        border.style.width = `${scroll / (sectionY.top + sectionHeight) * 5}em`;
+
+    // Fade-out
+    } else if (sectionY.top < 0) {
+        opacity.forEach(element => {
+            element.style.opacity = -1 * (scroll - sectionHeight) / (sectionHeight / 2) + 1;
+        });
+    }
+
+    // Outro section
+    let rate = inspired.dataset.rate;
+    let transform = (scroll * rate) / (outroHeight + outroY.top) * 50 - 75;
+    inspired.style.transform = `translateX(${transform}px)`;
+
+    let test = scroll / (outroY.top + outroHeight) - 1;
+    outroOpacity.style.opacity = test;
+
 });
